455338476da34ee40698d37b1910c90762c1b946
atitude-gustavosantos <gustavo.santos@atitude.com.br>
2019-05-23 19:19:45 -0300

    Criação de Logica.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './style.css';
+
+class App extends Component {
+  state = {
+    count: 1,
+    layout: 'vertical-layout',
+  };
+
+  addVideo() {
+    const element = document.querySelector('.col-participants');
+    const video = document.createElement('div');
+    video.setAttribute('id', 'subscriber');
+    video.setAttribute('class', 'video');
+    video.appendChild(document.createTextNode('video'));
+    element.appendChild(video);
+
+    this.setState({ count: element.childElementCount });
+  }
+
+  removeVideo() {
+    const element = document.getElementById('subscriber');
+    if (element) element.parentNode.removeChild(element);
+
+    this.setState({
+      count: document.querySelector('.col-participants').childElementCount,
+    });
+  }
+
+  setLayout(layout) {
+    this.setState({ layout });
+  }
+
+  render() {
+    const { count, layout } = this.state;
+
+    return (
+      <React.Fragment>
+        <div id="banner" />
+
+        <div id="box-player" className={count > 4 ? 'col-reverse' : ''}>
+          <div className={`col-participants ${layout}`}>
+            <div id="publisher" className="video">
+              Publisher
+            </div>
+          </div>
+          <div
+            id="col-presentation"
+            className={layout == 'camera-presentation' ? 'start' : ''}
+          >
+            <div id="presentation">Presentation</div>
+          </div>
+        </div>
+
+        <div id="control">
+          <a onClick={() => this.addVideo()}>+ Video</a>
+          <a onClick={() => this.removeVideo()}>- Video</a>
+          <a onClick={() => this.setLayout('vertical-layout')}>vertical</a>
+          <a onClick={() => this.setLayout('horizontal-layout')}>horizontal</a>
+          <a onClick={() => this.setLayout('side-by-side')}>side-by-side</a>
+          <a onClick={() => this.setLayout('camera-presentation')}>
+            presentation
+          </a>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
